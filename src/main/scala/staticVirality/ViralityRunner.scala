8a797a43a5bc8ad3e4759c212cf1cd92bebcf586
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-02-17 10:57:49 +0100

    Execution

--- src/main/scala/staticVirality/ViralityRunner.scala
+++ src/main/scala/staticVirality/ViralityRunner.scala
@@ -155,10 +155,10 @@ object ViralityRunner extends App {
 
     var a: Int = 0;
 
-    var viralityEvolution: DataFrame = cascadesFiltered.
-      withColumn("size",col("duration")+lit(1)).
-      select("cascade","size")
-//    var viralityEvolution: DataFrame = cascadesFiltered.select("cascade")
+//    var viralityEvolution: DataFrame = cascadesFiltered.
+//      withColumn("size",col("duration")+lit(1)).
+//      select("cascade","size")
+    var viralityEvolution: DataFrame = cascadesFiltered.select("cascade")
 
     for (i <- lowestBound to highestBound by increment) {
 //    for (i <- lowestBound to 4 by increment) {
