4601749eb384b2b1a64be525f3bc92e0a72fb8d6
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-02-22 11:34:22 +0100

    Execution

--- src/main/scala/staticVirality/ViralityRunner.scala
+++ src/main/scala/staticVirality/ViralityRunner.scala
@@ -283,7 +283,7 @@ object ViralityRunner extends App {
 
   //Dynamic
   dataset.cache()
-  val dynamicResult = incrementalWindowExecution((2,10),1,dataset)
+  val dynamicResult = incrementalWindowExecution((2,3),1,dataset)
 
   // Save results in a file
 
@@ -293,7 +293,7 @@ object ViralityRunner extends App {
 //  writeResults(nonHatefulResult,
 //    "/home/rcalzada/output/generations_8_nt/non-hateful","csv")
     writeResults(dynamicResult,
-      "hdfs://com31.dit.upm.es:9000/data/rcalzada/results/10_inc_1","csv")
+      "hdfs://com31.dit.upm.es:9000/data/rcalzada/results/3_inc_1","csv")
 //    writeResults(dynamicResult,
 //      "hdfs://com31.dit.upm.es:9000/data/rcalzada/results/test","csv")
 
